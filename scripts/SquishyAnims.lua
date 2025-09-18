fe369f8ca87d5af18ac30a7ee743c1bdb299b210
Total <39064108+TotalTakeover@users.noreply.github.com>
2025-09-17 19:47:14 -0700

    Arm movement toggle fix
    - Fixed arms logic being flipped
      - Arms were supposed to stop moving when octopus was in big mode, instead they only moved when in big mode. This has been changed to the correct state
    - Thank you to @qwsxedcrfv for finding this bug

--- scripts/SquishyAnims.lua
+++ scripts/SquishyAnims.lua
@@ -130,7 +130,7 @@ function events.TICK()
 	local crossR      = rightItem.tag and rightItem.tag["Charged"] == 1
 	
 	-- Arm movement overrides
-	local armShouldMove = tailScale.isLarge
+	local armShouldMove = not tailScale.isLarge
 	
 	-- Control targets based on variables
 	leftArmLerp.target  = (armsMove or armShouldMove or leftSwing  or bow or ((crossL or crossR) or (using and usingL ~= "NONE"))) and 1 or 0
