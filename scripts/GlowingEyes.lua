551768438e2713a739cd941244202806a9d538b0
Total <39064108+TotalTakeover@users.noreply.github.com>
2026-09-08 22:51:20 -0700

    Spellchecking
    - I promise I know how to spell, it's just hard when scripting.

--- scripts/GlowingEyes.lua
+++ scripts/GlowingEyes.lua
@@ -106,7 +106,7 @@ end
 
 -- Required scripts
 local s, pageNav, acts, colors = pcall(require, "scripts.ActionWheel")
-if not s then return end -- Kills script early if ActionWheel.lua isnt found
+if not s then return end -- Kills script early if ActionWheel.lua isn't found
 pcall(require, "scripts.GlowingTail") -- Tries to find script, not required
 
 -- Pages
--- scripts/GlowingTail.lua
+++ scripts/GlowingTail.lua
@@ -198,7 +198,7 @@ end
 
 -- Required script
 local s, pageNav, acts, colors = pcall(require, "scripts.ActionWheel")
-if not s then return end -- Kills script early if ActionWheel.lua isnt found
+if not s then return end -- Kills script early if ActionWheel.lua isn't found
 
 -- Pages
 local parentPage = action_wheel:getPage("Main")
